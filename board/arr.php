870d9ba2d0c9b96a86c414ac97e83971612fc9c0
inod-code <inoue.daisuke.00@gmail.com>
2019-06-30 11:46:29 +0900

    if文,構造体の作成

---
+++ board/arr.php
@@ -0,0 +1,124 @@
+<?php
+ // アクセスURL：http://localhost/board/arr.php
+ 
+ $var=1;
+ $var2='1';
+ $name='井上';
+ var_dump($var);
+ var_dump($var2);
+ var_dump($name);
+
+ echo '<br>';
+// 配列：arrayで定義、値はカンマで区切る
+$arr=array(
+    'inoue',
+    'daisuke',
+    'tokyo',
+    'PHP'
+);
+var_dump($arr);
+echo '<br>';
+echo '<br>';
+
+echo $arr[0];
+echo '<br>';
+echo '<br>';
+
+echo $arr[1];
+echo '<br>';
+echo '<br>';
+
+$arr[4] = 'man';
+var_dump($arr);
+echo '<br>';
+echo '<br>';
+
+$arr[2]='tokyo';
+var_dump($arr);
+echo '<br>';
+echo '<br>';
+
+$label_arr=array(
+    'family_name'=>'inoue',
+    'first_name'=>'daisuke',
+    'pref'=>'tokyo',
+    'language'=>'PHP'
+);
+
+var_dump($label_arr);
+echo '<br>';
+echo '<br>';
+
+echo $label_arr['language'];
+echo '<br>';
+echo '<br>';
+
+$label_arr['hobby']='golf';
+var_dump($label_arr);
+echo '<br>';
+echo '<br>';
+
+$excel_arr=array(
+    array(
+        'family_name'=>'inoue',
+        'first_name'=>'daisuke',
+        'perf'=>'tokyo',
+        'language'=>'PHP'
+    ),
+    array(
+        'family_name'=>'yamada',
+        'first_name'=>'tarou',
+        'perf'=>'chiba',
+        'language'=>'C'
+    ),
+    array(
+        'family_name'=>'sato',
+        'first_name'=>'hanako',
+        'perf'=>'kanagawa',
+        'language'=>'Java'
+    )
+);
+
+var_dump($excel_arr);
+echo '<br>';
+echo '<br>';
+
+var_dump($excel_arr[1]);
+echo '<br>';
+echo '<br>';
+
+echo $excel_arr[0]['language'];
+var_dump($excel_arr);
+echo '<br>';
+echo '<br>';
+
+echo $excel_arr[1]['perf'];
+echo '<br>';
+echo '<br>';
+
+$excel_arr[1]['perf']='osaka';
+var_dump($excel_arr);
+echo '<br>';
+echo '<br>';
+
+$excel_arr[3]=array(
+    'family_name'=> 'suzuki',
+    'first_name'=>'jiro',
+    'perf'=>'saitama',
+    'language'=> 'Perl'
+);
+var_dump($excel_arr);
+echo '<br>';
+echo '<br>';
+
+$arr=array(
+    'family_name'=>'suzuki',
+    'first_name'=>'jiro',
+    'perf'=>'saitama',
+    'language'=>'Perl'
+);
+
+array_push($excel_arr,$arr);
+var_dump($excel_arr);
+
+//コメント追加
---
+++ contact/if.php
@@ -0,0 +1,81 @@
+<?php
+// アクセスURL：http://localhost/contact/if.php
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<title>1.if文振り返り</title>
+</head>
+<body>
+
+<?php
+echo 'イノデー';
+echo '<br>';
+echo 'inod';
+echo '<br>';
+
+$x='10';
+
+// ==大体同じ
+// ===ちゃんと（変数の型が）同じ
+echo '<br>';
+
+$id ='';
+if($id===''){
+    echo 'empty';
+}else{
+    echo 'not empty';
+}
+echo '<br>';
+
+if($x==='10'){
+    echo '10';
+}else{
+    echo 'not 10';
+}
+echo '<br><br>';
+
+// 下記を変更すると条件が変わる
+$my_score = 90; //数字
+
+if($my_score>='80'){
+    echo '優';
+}elseif($my_score>='60'){
+    echo '良';
+}elseif($my_score>='40'){
+    echo '可';
+}else{
+    echo '不可';
+}
+echo '<br>';
+
+$a=10;
+$c=7;
+
+// &&(アンパサンド)→両方とも一致(AND条件)
+// ||(バーティカルバー/ライン、縦線)→片方満たせばOK（OR条件）
+if ($a===10 || $c===8){
+    echo 'OK';
+}else{
+    echo 'NG';
+}
+echo '<br>';
+
+// !==(!=)否定
+// $a === '' ←空白、未入力
+$a = '1';
+if(empty($a) === true){
+    echo '空(です）';
+}
+echo '<br>';
+if($a !== ''){
+    echo '値が入っています。';
+} else {
+    echo '空白です';
+}
+?>
+</body>
+</html>
